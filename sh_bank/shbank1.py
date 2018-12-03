037d0f219ec1c3219512e6b9b18c0bcdfa9cf57d
5646ksj <5646ksj@naver.com>
2018-12-03 11:48:53 -0800

    sh_bank form is different

---
+++ sh_bank/shbank1.py
@@ -0,0 +1,21 @@
+import re
+import requests
+s=requests.Session()
+req=s.get('https://bank.shinhan.com/index.jsp#020501010000')
+#req.encoding='euc-kr'
+html=req.text
+print(html)
+'''
+body=re.search('<div id="grd_list_1+.*?tabinde="0">', html, re.I|re.S)
+if(body is None) :
+	print("No <body> in html")
+	exit()
+body=body.group()
+print(body)
+'''
+#bodys=body.split()
+#text=re.sub('<.+?>','',body,0, re.I|re.S)
+#list=text.append()
+#print(list)
+#f=open('shlist.py','w')
+#f.write(text)
