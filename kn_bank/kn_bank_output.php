a8f20f8038370587176d937b8ea61bb13df202cd
5646ksj <5646ksj@naver.com>
2018-12-07 10:39:35 -0800

    kyungnam bank

---
+++ kn_bank/kn_bank_output.php
@@ -0,0 +1,36 @@
+<td class="last"><img src="/images/mibank/ko/JPY.png" alt="일본"></td>
+						<td class="first"><a href="/exchange/saving/index.php?currency=JPY">일본</a></td>
+						<td class="right JS_cash_buy">1011.49</td>
+						<td class="right JS_cash_buy">1.75%</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_t_m_send">1003.54</td>
+						<td class="right JS_t_m_send">0.95%</td>
+						<td class="right">994.10</td>
+						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="JPY"></a></td>
+					</tr>
+											<tr>
+						<td class="last"><img src="/images/mibank/ko/USD.png" alt="미국"></td>
+						<td class="first"><a href="/exchange/saving/index.php?currency=USD">미국</a></td>
+						<td class="right JS_cash_buy">1141.12</td>
+						<td class="right JS_cash_buy">1.75%</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_t_m_send">1132.15</td>
+						<td class="right JS_t_m_send">0.95%</td>
+						<td class="right">1121.50</td>
+						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="USD"></a></td>
+					</tr>
+										<tr>
+						<td class="last"><img src="/images/mibank/ko/EUR.png" alt="유럽연합"></td>
+						<td class="first"><a href="/exchange/saving/index.php?currency=EUR">유럽연합</a></td>
+						<td class="right JS_cash_buy">1297.67</td>
+						<td class="right JS_cash_buy">1.75%</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_cash_sell">-</td>
+						<td class="right JS_t_m_send">1287.47</td>
+						<td class="right JS_t_m_send">0.95%</td>
+						<td class="right">1275.36</td>
+						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="EUR"></a></td>
+					</tr>
+									</tbody>
