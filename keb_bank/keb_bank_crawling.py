b004e9fd7615d961bccc70cf5cc7ca215562ee63
giantbearkim <xodnd2494@naver.com>
2018-12-07 21:23:54 +0900

    add keb

---
+++ keb_bank/keb_bank_crawling.py
@@ -0,0 +1,10 @@
+import re
+import requests
+f = open('keb_bank_output.php','w')
+s=requests.Session()
+req=s.get('http://fx.kebhana.com/FER1101C.web?q=')
+html=req.text
+#print(html)
+body = re.search('<div id="gridPosition">.*?</table>',html,re.I|re.S)
+f.write(body.group())
+f.close
