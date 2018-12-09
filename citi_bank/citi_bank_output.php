76fc99e182889ca55782fc2b00081f58f99ac22d
5646ksj <5646ksj@naver.com>
2018-12-09 11:44:09 -0800

    crawling_final

--- citi_bank/citi_bank_output.php
+++ citi_bank/citi_bank_output.php
@@ -1,4 +1,61 @@
-<td class="last"><img src="/images/mibank/ko/CNY.png" alt="중국"></td>
+<table class="table_style01 exchange_table" border="1" cellspacing="0" summary="나라별 통화 환산리스트">
+				<caption>나라별 통화 환산리스트</caption>
+				<colgroup></colgroup>
+				<thead>
+					<tr>
+						<th scope="col" colspan="2" class="first">외화</th>
+						<th scope="col">
+							<a href="#none" id="JS_th1_cash_buy" class="" data-tab="cash_buy" style="display:inline-block;">현찰 살 때</a>
+							<div id="JS_layer1" class="layer_help_position" style="display: none;">
+                            	<a href="#none" class="JS_layer_toggle" data-layer-target="JS_layer_exchange_common1"><img src="/images/mibank/ko/ico_click_help_on.png" alt="도움말" class="table_tit_help"></a>
+								<p class="JS_layer_exchange_common1 layer_help5" style="display: none; width:175px;">
+									은행들이 고시하는 환율에는<br />은행마다 서로 다른 환전수수료가 포함되어 있습니다. 
+								</p>
+						    </div>
+						</th>
+						<th scope="col" id="JS_th2_cash_buy" class="percent txt_black">수수료율</th> 
+						<th scope="col">
+							<a href="#none" id="JS_th1_cash_sell" class="" data-tab="cash_sell" style="display:inline-block;">현찰 팔 때</a>
+							<div id="JS_layer2" class="layer_help_position" style="display: none;">
+                            	<a href="#none" class="JS_layer_toggle" data-layer-target="JS_layer_exchange_common2"><img src="/images/mibank/ko/ico_click_help_on.png" alt="도움말" class="table_tit_help"></a>
+								<p class="JS_layer_exchange_common2 layer_help5" style="display: none; width:175px;">
+									은행들이 고시하는 환율에는<br />은행마다 서로 다른 환전수수료가 포함되어 있습니다. 
+								</p>
+						    </div>
+						</th>
+						<th scope="col" id="JS_th2_cash_sell" class="percent">수수료율</th> 
+						<th scope="col">
+							<a href="#none" id="JS_th1_t_m_send" class="" data-tab="t_m_send" style="display:inline-block;">송금 보낼 때</a>
+							<div id="JS_layer3" class="layer_help_position" style="display: none;">
+								<a href="#none" class="JS_layer_toggle" data-layer-target="JS_layer_exchange_common3"><img src="/images/mibank/ko/ico_click_help_on.png" alt="도움말" class="table_tit_help"></a>
+								<p class="JS_layer_exchange_common3 layer_help5" style="display: none; width:175px;">
+									은행들이 고시하는 환율에는<br />은행마다 서로 다른 환전수수료가 포함되어 있습니다. 
+								</p>
+						    </div>
+						</th>  
+						<th scope="col" id="JS_th2_t_m_send" class="percent">수수료율</th>
+						<th scope="col">매매기준율</th>
+						<th scope="col" class="last"></th>
+					</tr> 
+				</thead>
+				<tbody>
+					<!-- 2017.06.26. J. udpate.
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					 -->
+					
+					<tr> 
+						<td class="last"><img src="/images/mibank/ko/CNY.png" alt="중국"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=CNY">중국</a></td>
 						<td class="right JS_cash_buy">175.22</td>
 						<td class="right JS_cash_buy">7.40%</td>
@@ -12,13 +69,13 @@
 											<tr>
 						<td class="last"><img src="/images/mibank/ko/JPY.png" alt="일본"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=JPY">일본</a></td>
-						<td class="right JS_cash_buy">1014.70</td>
-						<td class="right JS_cash_buy">2.01%</td>
-						<td class="right JS_cash_sell">974.84</td>
-						<td class="right JS_cash_sell">2.00%</td>
-						<td class="right JS_t_m_send">1007.12</td>
-						<td class="right JS_t_m_send">1.25%</td>
-						<td class="right">994.73</td>
+						<td class="right JS_cash_buy">1016.50</td>
+						<td class="right JS_cash_buy">2.00%</td>
+						<td class="right JS_cash_sell">976.75</td>
+						<td class="right JS_cash_sell">1.99%</td>
+						<td class="right JS_t_m_send">1008.91</td>
+						<td class="right JS_t_m_send">1.24%</td>
+						<td class="right">996.58</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="JPY"></a></td>
 					</tr>
 										<tr>
@@ -36,63 +93,64 @@
 										<tr>
 						<td class="last"><img src="/images/mibank/ko/EUR.png" alt="유럽연합"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=EUR">유럽연합</a></td>
-						<td class="right JS_cash_buy">1305.02</td>
-						<td class="right JS_cash_buy">2.24%</td>
-						<td class="right JS_cash_sell">1248.02</td>
+						<td class="right JS_cash_buy">1309.50</td>
+						<td class="right JS_cash_buy">2.23%</td>
+						<td class="right JS_cash_sell">1252.41</td>
 						<td class="right JS_cash_sell">2.22%</td>
-						<td class="right JS_t_m_send">1292.35</td>
-						<td class="right JS_t_m_send">1.25%</td>
-						<td class="right">1276.39</td>
+						<td class="right JS_t_m_send">1296.79</td>
+						<td class="right JS_t_m_send">1.24%</td>
+						<td class="right">1280.88</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="EUR"></a></td>
 					</tr>
 										<tr>
 						<td class="last"><img src="/images/mibank/ko/GBP.png" alt="영국"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=GBP">영국</a></td>
-						<td class="right JS_cash_buy">1469.32</td>
+						<td class="right JS_cash_buy">1467.02</td>
 						<td class="right JS_cash_buy">2.56%</td>
-						<td class="right JS_cash_sell">1396.26</td>
+						<td class="right JS_cash_sell">1394.07</td>
 						<td class="right JS_cash_sell">2.54%</td>
-						<td class="right JS_t_m_send">1450.50</td>
+						<td class="right JS_t_m_send">1448.23</td>
 						<td class="right JS_t_m_send">1.25%</td>
-						<td class="right">1432.65</td>
+						<td class="right">1430.40</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="GBP"></a></td>
 					</tr>
 										<tr>
 						<td class="last"><img src="/images/mibank/ko/AUD.png" alt="호주"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=AUD">호주</a></td>
-						<td class="right JS_cash_buy">829.89</td>
-						<td class="right JS_cash_buy">2.58%</td>
-						<td class="right JS_cash_sell">788.30</td>
-						<td class="right JS_cash_sell">2.56%</td>
-						<td class="right JS_t_m_send">819.27</td>
-						<td class="right JS_t_m_send">1.27%</td>
-						<td class="right">808.99</td>
+						<td class="right JS_cash_buy">829.55</td>
+						<td class="right JS_cash_buy">2.56%</td>
+						<td class="right JS_cash_sell">788.40</td>
+						<td class="right JS_cash_sell">2.53%</td>
+						<td class="right JS_t_m_send">818.92</td>
+						<td class="right JS_t_m_send">1.24%</td>
+						<td class="right">808.87</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="AUD"></a></td>
 					</tr>
 					
 					<tr> 
 						<td class="last"><img src="/images/mibank/ko/CAD.png" alt="캐나다"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=CAD">캐나다</a></td>
-						<td class="right JS_cash_buy">859.33</td>
-						<td class="right JS_cash_buy">2.55%</td>
-						<td class="right JS_cash_sell">816.68</td>
-						<td class="right JS_cash_sell">2.54%</td>
-						<td class="right JS_t_m_send">848.32</td>
-						<td class="right JS_t_m_send">1.24%</td>
-						<td class="right">837.94</td>
+						<td class="right JS_cash_buy">864.61</td>
+						<td class="right JS_cash_buy">2.54%</td>
+						<td class="right JS_cash_sell">821.89</td>
+						<td class="right JS_cash_sell">2.53%</td>
+						<td class="right JS_t_m_send">853.54</td>
+						<td class="right JS_t_m_send">1.22%</td>
+						<td class="right">843.22</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="CAD"></a></td>
 					</tr>
 						
 					<tr> 
 						<td class="last"><img src="/images/mibank/ko/CHF.png" alt="스위스"></td>
 						<td class="first"><a href="/exchange/saving/index.php?currency=CHF">스위스</a></td>
-						<td class="right JS_cash_buy">1158.87</td>
+						<td class="right JS_cash_buy">1163.32</td>
 						<td class="right JS_cash_buy">2.54%</td>
-						<td class="right JS_cash_sell">1101.51</td>
+						<td class="right JS_cash_sell">1105.74</td>
 						<td class="right JS_cash_sell">2.54%</td>
-						<td class="right JS_t_m_send">1144.03</td>
+						<td class="right JS_t_m_send">1148.42</td>
 						<td class="right JS_t_m_send">1.22%</td>
-						<td class="right">1130.19</td>
+						<td class="right">1134.53</td>
 						<td class="last"><a href="#none" class="JS_chart_btn ico_graph" data-code="CHF"></a></td>
 					</tr>
 										</tbody>
+			</table>
---
+++ kn_bank/kn_bank_sb.py
@@ -0,0 +1,23 @@
+import re
+f=open('kn_bank_list.py','r')
+#print(f.readable(), f.writable())
+html=f.read()
+parse=re.sub("['|'|' '|\n]","",html)
+parse1=parse.replace("]","").replace("[",",")
+#parse2=parse.replace("","")
+#parse=html.replace("(\n)","")
+#parse=html.replace("[","")
+#parse=html.replace("]","")
+#text=html.findall("'.*'",html)
+parse1=parse1.split(',')
+#print(parse1)
+list=[[0]*3 for i in range(3)]
+for i in range(0,3):
+        list[i][0]=parse1[8*i+1]
+        list[i][1]=parse1[8*i+2]
+        list[i][2]=parse1[8*i+4]
+#print(list)
+fo=open('kn_bank_sb.php','w')
+fo.write(','.join(map(str,list)))
+fo.close()
+
