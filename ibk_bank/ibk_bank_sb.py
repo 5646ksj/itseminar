59ee00605ee92d60882a8cb92d530878a149cdec
5646ksj <5646ksj@naver.com>
2018-12-09 11:45:52 -0800

    crawling final

---
+++ ibk_bank/ibk_bank_sb.py
@@ -0,0 +1,23 @@
+import re
+f=open('ibk_bank_list.py','r')
+#print(f.readable(), f.writable())
+html=f.read()
+parse=re.sub("['|'|' '|\n]","",html)
+parse1=parse.replace("]","").replace("[",",")
+#parse2=parse.replace("","")
+#parse=html.replace("(\n)","")
+#parse=html.replace("[","")
+#parse=html.replace("]","")
+#text=html.findall("'.*'",html)
+parse1=parse1.split(',')
+#print(parse1)
+list=[[0]*3 for i in range(42)]
+for i in range(0,42):
+        list[i][0]=parse1[8*i+1]
+        list[i][1]=parse1[8*i+2]
+        list[i][2]=parse1[8*i+4]
+#print(list)
+fo=open('ibk_bank_sb.php','w')
+fo.write(','.join(map(str,list)))
+fo.close()
+
---
+++ st_ch_bank/st_ch_bank_sb.py
@@ -0,0 +1,23 @@
+import re
+f=open('st_ch_bank_list.py','r')
+#print(f.readable(), f.writable())
+html=f.read()
+parse=re.sub("['|'|' '|\n]","",html)
+parse1=parse.replace("]","").replace("[",",")
+#parse2=parse.replace("","")
+#parse=html.replace("(\n)","")
+#parse=html.replace("[","")
+#parse=html.replace("]","")
+#text=html.findall("'.*'",html)
+parse1=parse1.split(',')
+#print(parse1)
+list=[[0]*3 for i in range(21)]
+for i in range(0,21):
+        list[i][0]=parse1[8*i+1]
+        list[i][1]=parse1[8*i+2]
+        list[i][2]=parse1[8*i+4]
+#print(list)
+fo=open('st_ch_bank_sb.php','w')
+fo.write(','.join(map(str,list)))
+fo.close()
+
