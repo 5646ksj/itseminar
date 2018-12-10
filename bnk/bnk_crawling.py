e9c8435ad718dccd38c4b224147bf86f75c2b57f
giantbearkim <xodnd2494@naver.com>
2018-12-10 21:17:29 +0900

    chang

--- bnk/bnk_crawling.py
+++ bnk/bnk_crawling.py
@@ -1,16 +1,9 @@
 import re
 import requests
-s=requests.Session()
 f = open('bnk_output.php','w')
+s=requests.Session()
 req=s.get('https://www.busanbank.co.kr/ib20/mnu/FPMFRX206001001#wrap')
-#req.encoding='euc-kr'
 html=req.text
-body=re.search('<tr class="txt-r".*?</tbody>', html, re.I|re.S)
-if(body is None) :
-        print("No <body> in html")
-        exit()
-body=body.group()
-#print(body)
-f.write(body)
+body = re.search('<table cellspacing="0" class="tbl-type2" id="resultTable">.*?/table>',html,re.I|re.S)
+f.write(body.group())
 f.close
-
--- homepage/thb.php
+++ homepage/thb.php
@@ -1,5 +1,5 @@
 <?php
-$src = "../money/usd.php";
+$src = "../money/thb.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
