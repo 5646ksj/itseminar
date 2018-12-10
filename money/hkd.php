491c2286f6c182ec19920586554a06d27dd8b1e5
5646ksj <5646ksj@naver.com>
2018-12-10 04:23:14 -0800

    hkd.php last

--- money/hkd.php
+++ money/hkd.php
@@ -37,35 +37,62 @@ for($i=0;$i<8;$i++){
 	$jj_find=strpos($jj_bankar[$i][0],"홍콩");
 	
 	if($wr_find!==false){
-		echo "우리은행", $wr_bankar[$i][1] , $wr_bankar[$i][2];
+		$wr_l=$wr_bankar[$i][1];
+		$wr_r=$wr_bankar[$i][2];
 	}
 	if($sh_find!==false){
-                echo "신한은행", $sh_bankar[$i][1] , $sh_bankar[$i][2];
+                $sh_l=$sh_bankar[$i][1];
+                $sh_r=$sh_bankar[$i][2];
         }
 	if($nh_find!==false){
-                echo"농협은행", $nh_bankar[$i][1] , $nh_bankar[$i][2];
+                $sh_l=$sh_bankar[$i][1];
+                $sh_r=$sh_bankar[$i][2];
         }
 	if($ibk_find!==false){
-                echo "기업은행",$ibk_bankar[$i][1] , $ibk_bankar[$i][2];
+                $ibk_l=$ibk_bankar[$i][1];
+                $ibk_r=$ibk_bankar[$i][2];
         }
 	if($st_ch_find!==false){
-                echo "standard_chartered은행", $st_ch_bankar[$i][1] , $st_ch_bankar[$i][2];
+                $st_ch_l=$st_ch_bankar[$i][1];
+                $st_ch_r=$st_ch_bankar[$i][2];
         }
 //	if($citi_find!==false){
-//		echo"씨티은행", $citi_bankar[$i][1] , $citi_bankar[$i][2];
+//		$citi_l=$citi_bankar[$i][1];
+//              $citi_r=$citi_bankar[$i][2];
 //        }
 	if($shyup_find!==false){
-                echo "수협",$shyup_bankar[$i][1] , $shyup_bankar[$i][2];
+                $shyup_l=$shyup_bankar[$i][1];
+                $shyup_r=$shyup_bankar[$i][2];
         }
 	if($dg_find!==false){
-                echo "대구은행",$dg_bankar[$i][1] , $dg_bankar[$i][2];
+                $dg_l=$dg_bankar[$i][1];
+                $dg_r=$dg_bankar[$i][2];
         }
 	if($jb_find!==false){
-                echo "전북은행",$jb_bankar[$i][1] , $jb_bankar[$i][2];
+                $jb_l=$jb_bankar[$i][1];
+                $jb_r=$jb_bankar[$i][2];
         }
 	if($jj_find!==false){
-                echo "제주은행",$jj_bankar[$i][1] , $jj_bankar[$i][2];
+                $jj_l=$jj_bankar[$i][1];
+                $jj_r=$jj_bankar[$i][2];
         }
 }
 
 ?>
+<html>
+        <head>
+        </head>
+
+        <body>
+                <table>
+                           <tr><td></td><td>살때</td><td>팔 때</td><td></td></tr>
+      <tr><td>우리은행</td><td><?php echo $wr_l;?> </td><td><?php echo $wr_r;?> </td><td></td></tr>
+      <tr><td>신한은행</td><td><?php echo $sh_l;?> </td><td><?php echo $sh_r;?> </td><td></td></tr>
+      <tr><td>농협</td><td><?php echo $nh_l;?> </td><td><?php echo $nh_r;?> </td><td></td></tr>
+      <tr><td>SC 은행</td><td><?php echo $st_ch_l;?> </td><td><?php echo $st_ch_r;?> </td><td></td></tr>
+      <tr><td>수협</td><td><?php echo $shyup_l;?> </td><td><?php echo $shyup_r;?> </td><td></td></tr>
+      <tr><td>전북은행</td><td><?php echo $jb_l;?> </td><td><?php echo $jb_r;?> </td><td></td></tr>
+      <tr><td>제주은행</td><td><?php echo $jj_l;?> </td><td><?php echo $jj_r;?> </td><td></td></tr>
+                </table>
+        </body>
+</html>
